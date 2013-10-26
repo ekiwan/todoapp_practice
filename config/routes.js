53e7a3cbef43545f188ff37149f5a4f74d2025cf
Hack Reactor Students <github@hackreactor.com>
2013-10-26 11:38:05 -0700

    make server actually work and serve index.html

--- config/routes.js
+++ config/routes.js
@@ -17,12 +17,11 @@ module.exports = function(app) {
   app.post('tasks', task.create);
   app.get('/tasks/:id', task.get);
   app.put('/tasks/:id', task.change);
-  app.delete('/tasks/:id', task.delete);
+  app['delete']('/tasks/:id', task['delete']);
   app.options('*', function(req,res){
     res.set(defaultCorsHeaders);
     res.end('');
   });
 
   app.use(express.static(path.join(__dirname, '../public')));
-
 };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var app = require('express')();
-require('./config')(app);
+require('./config/db.js')(app);
+require('./config/routes.js')(app);
 var port = 8888;
 app.listen(port);
 console.log('Listening on port: ' + port);
