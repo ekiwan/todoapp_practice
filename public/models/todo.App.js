18353f3c5c8d5e849746c28be49cc22a6f6d9d7c
Hack Reactor Students <github@hackreactor.com>
2013-10-26 12:49:49 -0700

    make gooder

--- public/models/todo.App.js
+++ public/models/todo.App.js
@@ -1,7 +1,5 @@
 todo.App = Backbone.Model.extend({
   initialize: function(params) {
     this.tasks = new todo.Tasks();
-    // var task = new todo.Task({description: 'Your first todo!'});
-    // this.tasks.add(task);
   }
 });
